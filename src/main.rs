e7e65c09304d6bfee57d42156f78bd3c0fdb2508
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2016-12-19 21:25:11 +1100

    Beginning argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
+extern crate clap;
+
+use clap::{App};
 
 fn main() {
-  use std::collections::VecDeque;
-  let mut buf = VecDeque::new();
-  buf.push_back(3);
-  buf.push_back(4);
-  buf.push_back(5);
-  loop {
-    match buf.pop_front() {
-      Some(x) => println!("{}", x),
-      None => break,
-    }
-  }
+
+  let matches = App::new("SG")
+                        .version("0.1")
+                        .author("Sebastian Glazebrook, Guilherme Reis Campos")
+                        .about("The best fuzzy finder ever")
+                        .get_matches();
 }
