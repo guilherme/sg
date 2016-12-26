2062168d2c18abd20be46fccef53d83944cf2699
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-12-26 10:21:44 +0000

    Added more configurability to the cli
    --filter ( initial filter string )
    --input ( configure input value/source )
    --return ( configure return value )

--- src/sg/app.rs
+++ src/sg/app.rs
@@ -1,13 +1,26 @@
+pub enum InputSource {
+    Fixed(Vec<String>),
+    Stdin,
+}
+
+pub enum ReturnType {
+    All,
+    Selected,
+}
+
 pub struct App {
     headless: bool,
     unfiltered_results: Vec<String>,
     filtered_results: Vec<String>,
+    filter: String,
+    input: InputSource,
+    return_type: ReturnType,
 }
 
 impl App {
 
-    pub fn new(headless: bool) -> Self {
-        App { headless: headless, unfiltered_results: vec![], filtered_results: vec![] }
+    pub fn new(headless: bool, filter: String, input: InputSource, return_type: ReturnType) -> Self {
+        App { headless, unfiltered_results: vec![], filtered_results: vec![], filter, input, return_type }
     }
 
     pub fn start(&self) -> i32 {
