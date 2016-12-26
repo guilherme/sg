cc4b12b7707ced5e662a1d3260e3b0c5c14e83ac
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-12-26 10:40:19 +0000

    Added structure for App::start

--- src/sg/app.rs
+++ src/sg/app.rs
@@ -20,12 +20,29 @@ pub struct App {
 impl App {
 
     pub fn new(headless: bool, filter: String, input: InputSource, return_type: ReturnType) -> Self {
-        App { headless, unfiltered_results: vec![], filtered_results: vec![], filter, input, return_type }
+        App { headless, filter, input, return_type, unfiltered_results: vec![], filtered_results: vec![] }
     }
 
     pub fn start(&self) -> i32 {
-        if self.headless { println!("Running in headless mode!") }
+        self.create_ui();
+        self.start_filtering();
+        self.update_ui();
         0
+    }
+
+
+    //-------- private ---------//
+
+    fn create_ui(&self) {
+        if self.headless { println!("Running in headless mode!") }
+    }
+
+    fn start_filtering(&self) {
+        // TODO
+    }
+
+    fn update_ui(&self) {
+        // TODO
     }
 
 }
